225e086d26a55fdc7f52cebe84c656af9da5c0c7
zeel darji <zeeldarji05@gmail.com>
2025-10-26 00:54:54 +1100

    ALright, I have implemeneted the ParshingAlgogo whcih checks if the token follows the grammar rules or not. I haven't tested the code yet 😅. I will do that very soon and make some changes. I just wanted you to know what approach I am taking for this part of the assignemnt. So basically this program checks if the given list of token follows the gramamr rules or not. If it follows then it is added to the parseTree list.

--- Assignment 2 - Theory of Computer Science/src/ImplementedParseTable.java
+++ Assignment 2 - Theory of Computer Science/src/ImplementedParseTable.java
@@ -7,25 +7,32 @@ public class ImplementedParseTable {
 
 
     //the Hshmap templae which will store each rule with non-terminal value, terminal value and al the values it can change to.
-    private final Map<String , Map<String , List<String>>> tableRule = new HashMap<>();
+    private final Map<String , Map<Token.TokenType , List<String>>> tableRule = new HashMap<>();
 
     public void  NTerminalToTerminal(){
         tableRule.put("expr" , new HashMap<>());
         //now making the grammar rule for the expression which will consists of what value to assign to the expr.
 
-        tableRule.get("expr").put("NUMBER" , Arrays.asList("NUMBER"));
-        tableRule.get("expr").put("IDENTIFIER" , Arrays.asList("IDENTIFIER"));
-        tableRule.get("expr").put("(" ,Arrays.asList("(" , "paren-expr" , ")"));
-        tableRule.get("paren-expr").put("PLUS" , Arrays.asList("PLUS"));
-        tableRule.get("paren-expr").put("MINUS" , Arrays.asList("MINUS"));
-        tableRule.get("paren-expr").put("TIMES" , Arrays.asList("TIMES"));
-        tableRule.get("paren-expr").put("EQUALS" , Arrays.asList("EQUALS"));
-        tableRule.get("paren-expr").put("QUESTION" , Arrays.asList("QUESTION"));
-        tableRule.get("paren-expr").put("LAMBDA" , Arrays.asList("LAMBDA"));
-        tableRule.get("paren-expr").put("DELTA" , Arrays.asList("DELTA"));
+        tableRule.get("expr").put(Token.TokenType.NUMBER , Arrays.asList("NUMBER"));
+        tableRule.get("expr").put(Token.TokenType.IDENTIFIER , Arrays.asList("IDENTIFIER"));
+        tableRule.get("expr").put(Token.TokenType.LPAREN ,Arrays.asList("(" , "paren-expr" , ")"));
+        tableRule.get("paren-expr").put(Token.TokenType.PLUS , Arrays.asList("PLUS"));
+        tableRule.get("paren-expr").put(Token.TokenType.MINUS , Arrays.asList("MINUS"));
+        tableRule.get("paren-expr").put(Token.TokenType.MULT , Arrays.asList("TIMES"));
+        tableRule.get("paren-expr").put(Token.TokenType.EQUALS , Arrays.asList("EQUALS"));
+        tableRule.get("paren-expr").put(Token.TokenType.CONDITIONAL , Arrays.asList("QUESTION"));
+        tableRule.get("paren-expr").put(Token.TokenType.LAMBDA , Arrays.asList("LAMBDA"));
+        tableRule.get("paren-expr").put(Token.TokenType.LET , Arrays.asList("LET"));
 
 
     }
+
+    //returnign the tableRule cause it got a private access:
+    public static Map<String , Map<Token.TokenType , List<String>>> getTableRule(){
+        return tableRule;
+    }
+
+
     //initiating the internat hashmap.
 
 
